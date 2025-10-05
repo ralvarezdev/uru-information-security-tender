f577f491a06ffdc1787e4f9a42ed230c88c290ba
Ramón Álvarez <86166683+ralvarezdev@users.noreply.github.com>
2025-10-05 18:44:59 -0400

    refactor: added --init parameter to git submodule update command

--- update_submodules.bat
+++ update_submodules.bat
@@ -1,27 +1,27 @@
 @echo off
 
 @echo Updating certificate-grpc submodule...
-git submodule update --remote certificate-grpc
+git submodule update --init --remote certificate-grpc
 @echo Updated certificate-grpc submodule.
 
 @echo Updating encrypter-grpc submodule...
-git submodule update --remote encrypter-grpc
+git submodule update --init --remote encrypter-grpc
 @echo Updated encrypter-grpc submodule.
 
 @echo Updating decrypter-grpc submodule...
-git submodule update --remote decrypter-grpc
+git submodule update --init --remote decrypter-grpc
 @echo Updated decrypter-grpc submodule.
 
 @echo Updating certificate-app submodule...
-git submodule update --remote certificate-app
+git submodule update --init --remote certificate-app
 @echo Updated certificate-app submodule.
 
 @echo Updating bidder-app submodule...
-git submodule update --remote bidder-app
+git submodule update --init --remote bidder-app
 @echo Updated bidder-app submodule.
 
 @echo Updating admin-app submodule...
-git submodule update --remote admin-app
+git submodule update --init --remote admin-app
 @echo Updated admin-app submodule.
 
 @echo Git submodules updated.
